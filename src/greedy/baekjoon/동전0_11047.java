7b0649aea75cd7398c079ebf3ca908d2b619d7a7
chiyongs <tlscldyd@gmail.com>
2021-03-04 16:45:20 +0900

    백준 그리디 동전0 11047

---
+++ src/greedy/baekjoon/동전0_11047.java
@@ -0,0 +1,25 @@
+package greedy.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 동전0_11047 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
+    int[] coins = new int[N];
+    for(int i=0; i<N; i++) {
+      coins[i] = Integer.parseInt(br.readLine());
+    }
+    int count = 0;
+    for(int i=N-1;i>=0;i--) {
+      count += K / coins[i];
+      K = K % coins[i];
+    }
+    System.out.println(count);
+  }
+}
