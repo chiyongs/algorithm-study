9a3805e0a6e8df55efeaab9ed2ced7925cfa19c6
chiyongs <tlscldyd@gmail.com>
2021-12-02 23:58:36 +0900

    특정문자뒤집기1_04

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public String solution(String str) {
-        String answer = str;
-        String[] s = answer.split(" ");
-        int m = Integer.MIN_VALUE;
-        for (String s1 : s) {
-            if (s1.length() > m) {
-                m = s1.length();
-                answer = s1;
+        String answer="";
+        int lt=0,rt=str.length()-1;
+        char[] c = str.toCharArray();
+        char[] s = new char[str.length()];
+        int j = 0;
+        for (int i =str.length()-1; i>=0; i--) {
+            if (Character.isAlphabetic(c[i])) s[j++]=c[i];
+        }
+        j=0;
+        for (int i =0 ; i<str.length(); i++) {
+            if (Character.isAlphabetic(c[i])) {
+                c[i] =s[j++];
             }
         }
+        answer = String.valueOf(c);
         return answer;
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        String str = kb.nextLine();
+        String str = kb.next();
         System.out.println(T.solution(str));
     }
 
---
+++ src/특정문자뒤집기1_04.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class 특정문자뒤집기1_04 {
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+//        for (String s : str) {
+//            StringBuilder sb = new StringBuilder(s);
+//            answer.add(sb.reverse().toString());
+//        }
+        for (String x : str) {
+            char[] c = x.toCharArray();
+            int lt=0,rt=x.length()-1;
+            while(lt<rt) {
+                char tmp = c[lt];
+                c[lt] = c[rt];
+                c[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            System.out.println(c.toString());
+            String s = String.valueOf(c);
+            answer.add(s);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        특정문자뒤집기1_04 T = new 특정문자뒤집기1_04();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for (int i = 0 ; i<n; i++) {
+            str[i] = kb.next();
+        }
+        for (String x : T.solution(n,str)) {
+            System.out.println(x);
+        }
+    }
+}
