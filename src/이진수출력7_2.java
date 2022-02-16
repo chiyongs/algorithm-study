73847303de5853892562a296595a65cbbd660ef7
chiyongs <tlscldyd@gmail.com>
2022-02-16 20:54:37 +0900

    이진수출력7_2

---
+++ src/이진수출력7_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class 이진수출력7_2 {
+	public String solution(int n) {
+		return Integer.toBinaryString(n);
+	}
+
+	public static void main(String[] args) {
+		이진수출력7_2 T = new 이진수출력7_2();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(T.solution(n));
+	}
+}
