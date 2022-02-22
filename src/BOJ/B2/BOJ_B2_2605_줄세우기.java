6f295d22e194d3f09993ad10e1def238faafaaf9
chiyongs <tlscldyd@gmail.com>
2022-02-22 13:47:56 +0900

    [백준] B2 2605 줄세우기

---
+++ src/BOJ/B2/BOJ_B2_2605_줄세우기.java
@@ -0,0 +1,28 @@
+package BOJ.B2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class BOJ_B2_2605_줄세우기 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		LinkedList<Integer> list = new LinkedList<>();
+				
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		for(int i=0;i<n;i++) {			
+			int k = Integer.parseInt(st.nextToken());			
+			list.add(i-k, i+1);
+		}
+		
+		for(int i=0;i<n;i++) {
+			sb.append(list.get(i)).append(" ");
+		}
+		System.out.println(sb.toString());
+		
+	}	
+}
