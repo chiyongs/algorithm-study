d06f696fa36d306244911b5e360492000e495599
chiyongs <tlscldyd@gmail.com>
2022-03-04 19:10:22 +0900

    [프로그래머스] DFS/BFS 타겟 넘버

---
+++ src/programmers/bfs/PGS_BFS_타겟넘버.java
@@ -0,0 +1,31 @@
+package programmers.bfs;
+
+import java.util.*;
+
+public class PGS_BFS_타겟넘버 {
+
+	public int solution(int[] numbers, int target) {
+		int answer = 0;
+
+		Queue<Integer[]> Q = new LinkedList<>();
+		Q.offer(new Integer[] { numbers[0], 0 });
+		Q.offer(new Integer[] { -numbers[0], 0 });
+
+		while (!Q.isEmpty()) {
+			Integer[] cur = Q.poll();
+			if (cur[0] == target && cur[1] == numbers.length - 1)
+				answer++;
+
+			if (cur[1] == numbers.length - 1)
+				continue;
+			int next = cur[1] + 1;
+			int p = cur[0] + numbers[next];
+			int m = cur[0] - numbers[next];
+
+			Q.offer(new Integer[] { p, next });
+			Q.offer(new Integer[] { m, next });
+		}
+
+		return answer;
+	}
+}
