df20c9523d1bb28f977ffdbbef5ee208529d8eb3
chiyongs <tlscldyd@gmail.com>
2024-03-21 21:32:04 +0900

    [BOJ] S3 2606 바이러스

--- src/boj/s3/BOJ_S3_2606_바이러스.java
+++ src/boj/s3/BOJ_S3_2606_바이러스.java
@@ -3,55 +3,55 @@ package boj.s3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
  * @author chiyongs
- * @since 2022. 6. 1.
+ * @since 2024. 3. 21.
  * @see https://www.acmicpc.net/problem/2606
- * @performance 11660	80
+ * @performance 11632	80
  * @category #DFS #BFS
  * @note 
  * 		 
  */
 public class BOJ_S3_2606_바이러스 {
-	
-	static int[][] graphs;
-	static int V,E,cnt;
-	static boolean[] visited;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static int n;
+	static boolean[] visited;
+	static int[][] arr;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		V = Integer.parseInt(br.readLine());
-		E = Integer.parseInt(br.readLine());
-		
-		graphs = new int[V+1][V+1];
-		visited = new boolean[V+1];
-		
-		for(int i=0;i<E;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+
+		arr = new int[n+1][n+1];
+		for (int i=0;i<m;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
-			graphs[a][b] = 1;
-			graphs[b][a] = 1;
+			arr[a][b] = 1;
+			arr[b][a] = 1;
 		}
-		
-		cnt = 0;
-		dfs(1);
-		
-		System.out.println(cnt);
 
+		visited = new boolean[n+1];
+		visited[1] = true;
+
+		dfs(1);
+		int answer = 0;
+		for (int i=2;i<=n;i++) {
+			if (visited[i]) answer++;
+		}
+		System.out.println(answer);
 	}
-	
-	static void dfs(int a) {
-		visited[a] = true;
-		
-		for(int i=1;i<=V;i++) {
-			if(!visited[i] && graphs[a][i]==1) {
-				cnt++;
+
+	public static void dfs(int node) {
+		for (int i=1;i<=n;i++) {
+			if (i != node && arr[node][i] == 1 && !visited[i]) {
+				visited[i] = true;
 				dfs(i);
-			}						
+			}
 		}
 	}
 
