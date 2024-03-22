2bad8dd8a8536722ac0bf5262a983c62260b3fed
chiyongs <tlscldyd@gmail.com>
2024-03-22 23:51:59 +0900

    [BOJ] S2 11724 연결 요소의 개수

--- src/boj/s2/BOJ_S2_11724_연결요소의개수.java
+++ src/boj/s2/BOJ_S2_11724_연결요소의개수.java
@@ -3,70 +3,79 @@ package boj.s2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
  * @author chiyongs
- * @since 2022. 6. 2.
- * @see https://www.acmicpc.net/problem/11724
- * @performance 137840	544
+ * @performance 137840    544
  * @category #DFS #BFS
- * @note 
- * 		 
+ * @note
+ * @see <a href="https://www.acmicpc.net/problem/11724">...</a>
+ * @since 2022. 6. 2.
  */
 public class BOJ_S2_11724_연결요소의개수 {
-	
-	static class Node {
-		int vertex;
-		Node link;
-		
-		public Node(int vertex, Node link) {
-			this.vertex = vertex;
-			this.link = link;
-		}
-		
-	}
-	
-	static Node[] nodes;
-	static boolean[] visited;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		nodes = new Node[N+1];
-		visited = new boolean[N+1];
-		
-		for(int i=0;i<M;i++) {
-			st = new StringTokenizer(br.readLine()," ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			nodes[a] = new Node(b,nodes[a]);
-			nodes[b] = new Node(a,nodes[b]);
-		}
-		
-		int cnt = 0;
-		for(int i=1;i<=N;i++) {
-			if(visited[i]) continue;
-			cnt++;
-			dfs(i);
-		}
-		
-		System.out.println(cnt);
+    static List<List<Integer>> nodes;
+    static boolean[] visited;
+    static int N, M;
 
-	}
-	
-	static void dfs(int num) {
-		visited[num] = true;
-		
-		for(Node temp = nodes[num];temp!=null;temp=temp.link) {
-			if(visited[temp.vertex]) continue;
-			
-			visited[temp.vertex] = true;
-			dfs(temp.vertex);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        int answer = 0;
 
+        nodes = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            nodes.add(new ArrayList<>());
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            nodes.get(a - 1).add(b - 1);
+            nodes.get(b - 1).add(a - 1);
+        }
+
+        visited = new boolean[N];
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                answer++;
+//                dfs(i);
+                bfs(i);
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static void bfs(int node) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
+        visited[node] = true;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            List<Integer> list = nodes.get(cur);
+            for (Integer n : list) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    q.offer(n);
+                }
+            }
+        }
+    }
+
+    private static void dfs(int cur) {
+        visited[cur] = true;
+        for (Integer n : nodes.get(cur)) {
+            if (!visited[n]) {
+                visited[n] = true;
+                dfs(n);
+            }
+        }
+    }
 }
