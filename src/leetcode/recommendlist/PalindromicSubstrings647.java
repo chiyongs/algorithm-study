13a18fee98cbe613bcffe9db4843616f1bfbdd97
chiyongs <tlscldyd@gmail.com>
2024-06-08 00:20:02 +0900

    [LeetCode] 647. Palindromic Substrings

--- src/leetcode/recommendlist/PalindromicSubstrings647.java
+++ src/leetcode/recommendlist/PalindromicSubstrings647.java
@@ -3,24 +3,45 @@ package leetcode.recommendlist;
 import java.util.*;
 
 public class PalindromicSubstrings647 {
+//    public int countSubstrings(String s) {
+//        int sLen = s.length();
+//        List<String> result = new ArrayList<>();
+//        for(int i=0;i<sLen;i++) {
+//            result.addAll(getPalindromes(s, i, i));
+//            result.addAll(getPalindromes(s, i, i+1));
+//        }
+//
+//        return result.size();
+//    }
+//
+//    List<String> getPalindromes(String s, int start, int end) {
+//        List<String> palindromes = new ArrayList<>();
+//        while(0 <= start && end < s.length() && s.charAt(start) == s.charAt(end)) {
+//            start--;
+//            end++;
+//            palindromes.add(s.substring(start+1, end));
+//        }
+//        return palindromes;
+//    }
+
     public int countSubstrings(String s) {
         int sLen = s.length();
-        List<String> result = new ArrayList<>();
+        int result = 0;
         for(int i=0;i<sLen;i++) {
-            result.addAll(getPalindromes(s, i, i));
-            result.addAll(getPalindromes(s, i, i+1));
+            result += getPalindromes(s, i, i);
+            result += getPalindromes(s, i, i+1);
         }
 
-        return result.size();
+        return result;
     }
 
-    List<String> getPalindromes(String s, int start, int end) {
-        List<String> palindromes = new ArrayList<>();
+    int getPalindromes(String s, int start, int end) {
+        int count = 0;
         while(0 <= start && end < s.length() && s.charAt(start) == s.charAt(end)) {
+            count++;
             start--;
             end++;
-            palindromes.add(s.substring(start+1, end));
         }
-        return palindromes;
+        return count;
     }
 }
