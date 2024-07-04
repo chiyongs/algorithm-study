f19987f060028c07ee7f8ba610acc82eb53d06a0
Chiyongs <66486860+chiyongs@users.noreply.github.com>
2024-07-04 19:45:04 +0900

    [LeetCode] 392. Is Subsequence

---
+++ src/leetcode/recommendlist/IsSubsequence392.java
@@ -0,0 +1,16 @@
+public class IsSubsequence392 {
+    public boolean isSubsequence(String s, String t) {
+        int si = 0;
+        if (s.length() < 1)
+            return true;
+        for (int i = 0; i < t.length(); i++) {
+            if (s.charAt(si) == t.charAt(i))
+                si++;
+
+            if (si == s.length())
+                return true;
+        }
+
+        return false;
+    }
+}
