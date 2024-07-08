56506eaae9b8dc4947da862caf5b90dad0d0f3c3
chiyongs <tlscldyd@gmail.com>
2024-07-08 21:51:34 +0900

    [LeetCode] 219. Contains Duplicate II

---
+++ src/leetcode/recommendlist/ContainsDuplicateII219.java
@@ -0,0 +1,16 @@
+package leetcode.recommendlist;
+
+import java.util.*;
+
+public class ContainsDuplicateII219 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k)
+                set.remove(nums[i - k - 1]);
+            if (!set.add(nums[i]))
+                return true;
+        }
+        return false;
+    }
+}
