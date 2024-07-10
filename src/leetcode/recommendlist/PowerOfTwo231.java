5aa1a409f445878dac34f9fd75d5407b0837825b
chiyongs <tlscldyd@gmail.com>
2024-07-10 21:12:35 +0900

    [LeetCode] 231. Power Of Two

---
+++ src/leetcode/recommendlist/PowerOfTwo231.java
@@ -0,0 +1,13 @@
+package leetcode.recommendlist;
+
+public class PowerOfTwo231 {
+    public boolean isPowerOfTwo(int n) {
+        for (int i = 0; i < 31; i++) {
+            int ans = (int) Math.pow(2, i);
+            if (ans == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
