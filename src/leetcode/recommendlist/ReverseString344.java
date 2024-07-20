f71f758366a81be104276c10843e816b34788dec
chiyongs <tlscldyd@gmail.com>
2024-07-20 19:38:14 +0900

    [LeetCode] 344. Reverse String

---
+++ src/leetcode/recommendlist/ReverseString344.java
@@ -0,0 +1,14 @@
+package leetcode.recommendlist;
+
+public class ReverseString344 {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length-1;
+        while (left < right) {
+            char ch = s[left];
+            s[left] = s[right];
+            s[right] = ch;
+            left++;
+            right--;
+        }
+    }
+}
