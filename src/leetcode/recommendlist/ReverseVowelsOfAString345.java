8bef762b6d6ff09da3a13a8dd6879f5e57afff5b
chiyongs <tlscldyd@gmail.com>
2024-07-24 22:16:12 +0900

    [LeetCode] 345. Reverse Vowels Of A String

---
+++ src/leetcode/recommendlist/ReverseVowelsOfAString345.java
@@ -0,0 +1,30 @@
+package leetcode.recommendlist;
+
+public class ReverseVowelsOfAString345 {
+    public String reverseVowels(String s) {
+        char[] word = s.toCharArray();
+        int start = 0;
+        int end = s.length() - 1;
+        String vowels = "aeiouAEIOU";
+
+        while (start < end) {
+            while (start < end && vowels.indexOf(word[start]) == -1) {
+                start++;
+            }
+
+            while (start < end && vowels.indexOf(word[end]) == -1) {
+                end--;
+            }
+
+            char temp = word[start];
+            word[start] = word[end];
+            word[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        String answer = new String(word);
+        return answer;
+    }
+}
