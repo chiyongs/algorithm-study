8e0e7abf32934b78d57e29cf9c48764b555d93e7
chiyongs <tlscldyd@gmail.com>
2024-08-05 23:49:01 +0900

    [LeetCode] 326. Power Of Three

---
+++ src/leetcode/recommendlist/PowerOfThree326.java
@@ -0,0 +1,13 @@
+package leetcode.recommendlist;
+
+public class PowerOfThree326 {
+    public boolean isPowerOfThree(int n) {
+        if(n == 0) return false;
+        while(n != 1){
+            int r = n%3;
+            n = n/3;
+            if(r != 0) return false;
+        }
+        return true;
+    }
+}
