fbd72e209fcc97a1e9210895e659f8d58da33171
chiyongs <tlscldyd@gmail.com>
2024-08-06 14:22:18 +0900

    [LeetCode] 342. Power Of Four

---
+++ src/leetcode/recommendlist/PowerOfFour342.java
@@ -0,0 +1,17 @@
+package leetcode.recommendlist;
+
+public class PowerOfFour342 {
+    public boolean isPowerOfFour(int n) {
+        for (int i = 0; i <= 15; i++) {
+            int powerOfFour = (int) Math.pow(4, i);
+
+            if (powerOfFour == n)
+                return true;
+
+            if (powerOfFour > n)
+                return false;
+        }
+
+        return false;
+    }
+}
