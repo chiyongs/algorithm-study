8dcb7f8e0a29617db827a31fa53cc062f05e607c
chiyongs <tlscldyd@gmail.com>
2024-08-07 23:19:21 +0900

    [LeetCode] 374. Guess Number Higher Of Lower

---
+++ src/leetcode/recommendlist/GuessNumberHigherOrLower374.java
@@ -0,0 +1,22 @@
+package leetcode.recommendlist;
+
+public class GuessNumberHigherOrLower374 {
+
+    static int guess(int mid) {
+        return 0;
+    }
+
+    public int guessNumber(int n) {
+        int start=0,end=n;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(guess(mid)==0)
+                return mid;
+            else if(guess(mid)==-1)
+                end=mid-1;
+            else
+                start=mid+1;
+        }
+        return -1;
+    }
+}
