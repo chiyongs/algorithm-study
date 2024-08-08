8ff1513d874e7437a760c79b2b629c6f93d39263
chiyongs <tlscldyd@gmail.com>
2024-08-08 12:58:25 +0900

    [LeetCode] 409. Longest Palindrome

---
+++ src/leetcode/recommendlist/LongestPalindrome409.java
@@ -0,0 +1,24 @@
+package leetcode.recommendlist;
+
+import java.util.*;
+
+public class LongestPalindrome409 {
+    public int longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        Set<Character> set = new HashSet<>();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (set.contains(s.charAt(i))) {
+                set.remove(s.charAt(i));
+                count++;
+            } else {
+                set.add(s.charAt(i));
+            }
+        }
+        if (!set.isEmpty())
+            return count * 2 + 1;
+        return count * 2;
+    }
+}
