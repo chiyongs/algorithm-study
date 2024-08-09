515913fedeb9e207d62e1f9f292c0bc29c3bfd7d
chiyongs <tlscldyd@gmail.com>
2024-08-09 16:03:44 +0900

    [LeetCode] 334. Increasing Triplet Subsequence

---
+++ src/leetcode/recommendlist/IncreasingTripletSubsequence334.java
@@ -0,0 +1,24 @@
+package leetcode.recommendlist;
+
+public class IncreasingTripletSubsequence334 {
+    public boolean increasingTriplet(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return false;
+        }
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= a) {
+                a = num;
+            } else if (num <= b) {
+                b = num;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
