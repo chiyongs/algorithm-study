2074277747fdf0bda6c85c7d752f0a71b299677d
chiyongs <tlscldyd@gmail.com>
2024-08-11 08:55:18 +0900

    [LeetCode] 287. Find The Duplicate Number

---
+++ src/leetcode/recommendlist/FindTheDuplicateNumber287.java
@@ -0,0 +1,16 @@
+package leetcode.recommendlist;
+
+import java.util.*;
+
+public class FindTheDuplicateNumber287 {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return num;
+            }
+            set.add(num);
+        }
+        return -1;
+    }
+}
