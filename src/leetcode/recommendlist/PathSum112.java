a0f4996edae4970d4c96fc1aaab573c8410073ff
chiyongs <tlscldyd@gmail.com>
2024-08-27 21:27:44 +0900

    [LeetCode] 112. Path Sum

---
+++ src/leetcode/recommendlist/PathSum112.java
@@ -0,0 +1,31 @@
+package leetcode.recommendlist;
+
+public class PathSum112 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+
+        if (root.left == null && root.right == null && sum - root.val == 0) return true;
+
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
