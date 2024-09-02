ad72fe70a7f0286dabca1994586bd17558e8e6ba
chiyongs <tlscldyd@gmail.com>
2024-09-02 20:43:58 +0900

    [LeetCode] 1636. Sort Array By Increasing Frequency

---
+++ src/leetcode/recommendlist/SortArrayByIncreasingFrequency1636.java
@@ -0,0 +1,20 @@
+package leetcode.recommendlist;
+
+import java.util.Arrays;
+
+public class SortArrayByIncreasingFrequency1636 {
+    public int[] frequencySort(int[] nums) {
+        int[] count = new int[201];
+        for (int num : nums) {
+            count[num + 100]++;
+        }
+        Integer[] numsArr = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+        Arrays.sort(numsArr, (a, b) -> {
+            if (count[a + 100] == count[b + 100]) {
+                return b - a;
+            }
+            return count[a + 100] - count[b + 100];
+        });
+        return Arrays.stream(numsArr).mapToInt(Integer::intValue).toArray();
+    }
+}
