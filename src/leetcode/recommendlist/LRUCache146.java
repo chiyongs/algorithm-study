9c61995cafb244576d579b1836a810e437ca8573
chiyongs <tlscldyd@gmail.com>
2024-09-22 00:18:31 +0900

    [LeetCode] 146. LRUCache

--- src/leetcode/recommendlist/LRUCache146.java
+++ src/leetcode/recommendlist/LRUCache146.java
@@ -6,19 +6,6 @@ import java.util.Map;
 
 public class LRUCache146 {
 
-    public static void main(String[] args) {
-        LRUCache lRUCache = new LRUCache(2);
-        lRUCache.put(1, 1); // cache is {1=1}
-        lRUCache.put(2, 2); // cache is {1=1, 2=2}
-        lRUCache.get(1);    // return 1
-        lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-        lRUCache.get(2);    // returns -1 (not found)
-        lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-        lRUCache.get(1);    // return -1 (not found)
-        lRUCache.get(3);    // return 3
-        lRUCache.get(4);    // return 4
-    }
-
     static class LRUCache {
 
         class Cache {
@@ -54,14 +41,22 @@ public class LRUCache146 {
         }
 
         public void put(int key, int value) {
-            if (map.size() >= capacity) {
-                Cache last = linkedList.getLast();
-                map.remove(last.key);
-                linkedList.removeLast();
+            Cache cache = map.getOrDefault(key, null);
+            if (cache == null) {
+                cache = new Cache(key, value);
+                if (map.size() >= capacity) {
+                    Cache last = linkedList.getLast();
+                    map.remove(last.key);
+                    linkedList.remove(last);
+                }
+                map.put(key, cache);
+                linkedList.addFirst(cache);
+            } else {
+                cache.value = value;
+                linkedList.remove(cache);
+                map.put(key, cache);
+                linkedList.addFirst(cache);
             }
-            Cache cache = new Cache(key, value);
-            map.put(key, cache);
-            linkedList.addFirst(cache);
         }
 
     }
