f735c87adbd624e99c94975b2cf6ae7787d9c5aa
chiyongs <tlscldyd@gmail.com>
2024-10-10 19:38:44 +0900

    [LeetCode] 222. Count Complete Tree Nodes

---
+++ src/leetcode/recommendlist/CountCompleteTreeNodes222.java
@@ -0,0 +1,31 @@
+package leetcode.recommendlist;
+
+public class CountCompleteTreeNodes222 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+}
