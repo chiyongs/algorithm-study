4cb43c8f90c409447974750ecd74c5321f78d8e7
chiyongs <tlscldyd@gmail.com>
2024-10-13 22:45:28 +0900

    [LeetCode] 645. Set Mismatch

---
+++ src/leetcode/recommendlist/SetMismatch645.java
@@ -0,0 +1,17 @@
+package leetcode.recommendlist;
+
+public class SetMismatch645 {
+    public int[] findErrorNums(int[] nums) {
+        boolean[] visited = new boolean[10001];
+        int duplicate = 0, sum = 0, n = nums.length;
+        for (int i: nums) {
+            if (visited[i]) {
+                duplicate = i;
+            }
+            visited[i] = true;
+            sum += i;
+        }
+        int nsum =  (n * (n+1)) / 2;
+        return new int[] {duplicate, duplicate + nsum - sum};
+    }
+}
