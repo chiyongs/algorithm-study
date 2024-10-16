2dc0391c28a83230e0ad81d4338dac2b79b2c701
chiyongs <tlscldyd@gmail.com>
2024-10-16 22:46:24 +0900

    [LeetCode] 653. Two Sum IV Input Is A BST

---
+++ src/leetcode/recommendlist/TwoSumIVInputIsABST653.java
@@ -0,0 +1,20 @@
+package leetcode.recommendlist;
+
+import java.util.*;
+
+public class TwoSumIVInputIsABST653 {
+    Set<Integer> set = new HashSet<>();
+
+    public boolean findTarget(TreeNode root, int k) {
+        if (root == null) {
+            return false;
+        }
+        if (set.contains(k-root.val)) {
+            return true;
+        }
+
+        set.add(root.val);
+
+        return findTarget(root.left,k) || findTarget(root.right,k);
+    }
+}
