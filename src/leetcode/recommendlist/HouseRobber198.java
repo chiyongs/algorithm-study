b11c09ca15708c8060cc8f632444b74983d537ec
chiyongs <tlscldyd@gmail.com>
2024-11-27 20:47:07 +0900

    [LeetCode] 198. House Robber using dp

--- src/leetcode/recommendlist/HouseRobber198.java
+++ src/leetcode/recommendlist/HouseRobber198.java
@@ -1,7 +1,25 @@
 package leetcode.recommendlist;
 
 public class HouseRobber198 {
-    public int rob(int[] nums) {
+
+    public int rob_useDp(int[] nums) {
+        int[] dp = new int[nums.length];
+        if (nums.length == 1) {
+            return nums[0];
+        } else if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[1], dp[0]);
+        for (int i=2;i<nums.length;i++) {
+            dp[i] = Math.max((nums[i] + dp[i-2]), dp[i-1]);
+        }
+
+        return dp[nums.length-1];
+    }
+
+
+    public int rob_useRecursive(int[] nums) {
         return rob(nums, nums.length - 1);
     }
 
