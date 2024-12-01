62443bc5ce8a9b185134336523a908476209bebf
chiyongs <tlscldyd@gmail.com>
2024-12-01 22:33:29 +0900

    [LeetCode] 201. Bitwise And of Numbers Range

---
+++ src/leetcode/recommendlist/BitwiseANDOfNumbersRange201.java
@@ -0,0 +1,16 @@
+package leetcode.recommendlist;
+
+public class BitwiseANDOfNumbersRange201 {
+    public int rangeBitwiseAnd(int m, int n) {
+        if(m == 0){
+            return 0;
+        }
+        int moveFactor = 1;
+        while(m != n){
+            m >>= 1;
+            n >>= 1;
+            moveFactor <<= 1;
+        }
+        return m * moveFactor;
+    }
+}
