1cf36daab70762a9647ea200234699ac0c80d2a3
chiyongs <tlscldyd@gmail.com>
2025-01-11 17:39:44 +0900

    [LeetCode] 456. 132 Pattern

---
+++ src/leetcode/recommendlist/P132Pattern456.java
@@ -0,0 +1,19 @@
+package leetcode.recommendlist;
+
+import java.util.*;
+
+public class P132Pattern456 {
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        int third = Integer.MIN_VALUE;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < third) return true;
+            while (!stack.isEmpty() && stack.peek() < nums[i]) {
+                third = stack.pop();
+            }
+            stack.push(nums[i]);
+        }
+        return false;
+    }
+}
