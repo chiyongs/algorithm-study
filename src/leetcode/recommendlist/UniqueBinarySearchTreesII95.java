0b0e798bcb05d8cea0b5a28a0480a1afec2865ed
chiyongs <tlscldyd@gmail.com>
2025-01-24 23:50:57 +0900

    [LeetCode] 95. Unique Binary Search Trees II

---
+++ src/leetcode/recommendlist/UniqueBinarySearchTreesII95.java
@@ -0,0 +1,58 @@
+package leetcode.recommendlist;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class UniqueBinarySearchTreesII95 {
+
+    public static void main(String[] args) {
+        UniqueBinarySearchTreesII95 u = new UniqueBinarySearchTreesII95();
+        u.generateTrees(3);
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        return generateSubtrees(1, n);
+    }
+
+    private List<TreeNode> generateSubtrees(int s, int e) {
+        List<TreeNode> res = new LinkedList<>();
+        if (s > e) {
+            res.add(null);
+            return res;
+        }
+
+        for (int i = s; i <= e; ++i) {
+            List<TreeNode> leftSubtrees = generateSubtrees(s, i - 1);
+            List<TreeNode> rightSubtrees = generateSubtrees(i + 1, e);
+
+            for (TreeNode left : leftSubtrees) {
+                for (TreeNode right : rightSubtrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+}
