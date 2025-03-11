856d37473f1e8a99aeb214bbb8997e400e7751c3
Chiyongs <66486860+chiyongs@users.noreply.github.com>
2025-03-11 23:17:50 +0900

    [LeetCode] 924. Goat Latin

---
+++ src/leetcode/recommendlist/GoatLatin924.java
@@ -0,0 +1,19 @@
+public String toGoatLatin(String sentence) {
+    String[] words = sentence.split(" ");
+    StringBuilder res =  new StringBuilder();
+    String vowels = "aeiouAEIOU";
+
+    for(int i=0; i<words.length; i++){
+        String word = words[i];
+
+        if(vowels.indexOf(word.charAt(0)) != -1){
+            word += "ma";
+        } else{
+            word = word.substring(1) + word.charAt(0) + "ma";
+        }
+        word += "a".repeat(i+1);
+          
+        res.append(word).append(" ");
+    }
+    return res.toString().trim();
+}
