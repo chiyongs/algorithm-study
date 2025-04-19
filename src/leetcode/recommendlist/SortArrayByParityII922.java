2abd41b0d056bd2426ce3e6bd25008121771d983
chiyongs <tlscldyd@gmail.com>
2025-04-19 22:15:36 +0900

    [LeetCode] 922. Sort Array By Parity II

---
+++ src/leetcode/recommendlist/SortArrayByParityII922.java
@@ -0,0 +1,22 @@
+package leetcode.recommendlist;
+
+public class SortArrayByParityII922 {
+    public int[] sortArrayByParityII(int[] nums) {
+        int even = 0;
+        int odd = 1;
+
+        while (even < nums.length && odd < nums.length) {
+            if (nums[even] %2 != 0) {
+                int temp = nums[even];
+                nums[even] = nums[odd];
+                nums[odd] = temp;
+
+                odd += 2 ;
+            } else {
+                even += 2 ;
+            }
+        }
+
+        return nums;
+    }
+}
