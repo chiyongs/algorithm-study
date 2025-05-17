24e5da7d5b5735ae52b64a96e0886719b78af59e
chiyongs <tlscldyd@gmail.com>
2025-05-17 11:29:02 +0900

    [LeetCode] 521. Longest Uncommon Subsequence

---
+++ src/leetcode/recommendlist/LongestUncommonSubsequenceI521.java
@@ -0,0 +1,13 @@
+package leetcode.recommendlist;
+
+public class LongestUncommonSubsequenceI521 {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) return -1;
+
+        if (a.length() > b.length()) {
+            return a.length();
+        } else {
+            return b.length();
+        }
+    }
+}
