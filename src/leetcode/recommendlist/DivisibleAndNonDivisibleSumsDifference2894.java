c8be99bcb9e6c7af32e30a94cd6b5fa49a2d3a6e
chiyongs <tlscldyd@gmail.com>
2025-06-08 22:35:22 +0900

    [LeetCode] 2894. Divisible and Non Divisible Sums Difference

---
+++ src/leetcode/recommendlist/DivisibleAndNonDivisibleSumsDifference2894.java
@@ -0,0 +1,15 @@
+package leetcode.recommendlist;
+
+public class DivisibleAndNonDivisibleSumsDifference2894 {
+    public int differenceOfSums(int n, int m) {
+        int result = 0;
+        for (int i=1;i<=n;++i) {
+            if (i % m == 0) {
+                result -= i;
+            } else {
+                result += i;
+            }
+        }
+        return result;
+    }
+}
