3325d00c954512e89e96d4872a290b6d1b3e6611
chiyongs <tlscldyd@gmail.com>
2025-07-26 23:12:38 +0900

    [LeetCode] 1588. Sum of All Odd Length Subarrays

---
+++ src/leetcode/recommendlist/SumOfAllOddLengthSubarrays1588.kt
@@ -0,0 +1,13 @@
+package leetcode.recommendlist
+
+class SumOfAllOddLengthSubarrays1588 {
+    fun sumOddLengthSubarrays(arr: IntArray): Int {
+        var oddSum = 0
+
+        for (i in 1..arr.size step 2) {
+            oddSum += arr.toList().windowed(i).map { it.sum() }.sum()
+        }
+
+        return oddSum
+    }
+}
