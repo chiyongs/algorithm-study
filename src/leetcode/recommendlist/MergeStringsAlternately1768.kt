37450d61221deae2b7a1cf7aacf166a055c7a6cf
chiyongs <tlscldyd@gmail.com>
2025-08-12 22:08:06 +0900

    [LeetCode] 1768. Merge Strings Alternately

---
+++ src/leetcode/recommendlist/MergeStringsAlternately1768.kt
@@ -0,0 +1,21 @@
+package leetcode.recommendlist
+
+class MergeStringsAlternately1768 {
+    fun mergeAlternately(word1: String, word2: String): String {
+        val sb = StringBuilder()
+        var i1 = 0
+        var i2 = 0
+        while (i1 < word1.length && i2 < word2.length) {
+            sb.append(word1[i1++]).append(word2[i2++])
+        }
+
+        if (i1 < word1.length) {
+            sb.append(word1.substring(i1))
+        }
+        if (i2 < word2.length) {
+            sb.append(word2.substring(i2))
+        }
+
+        return sb.toString()
+    }
+}
